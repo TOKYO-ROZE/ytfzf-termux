69b0646e22e3e3ac764986f162f9089249e49b0f
HexHelix-xx <rokuhara@protonmail.ch>
2022-01-19 23:32:52 +0900

    ---

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 run_run() {
     printf "[*] installing the ytfzf-termux"
-    cp -v ./ytfzf /data/data/com.termux/files/usr/bin/
-    if [ type "ytfzf" > /dev/null 2>&1 ]; then
+    termux_bin="/data/data/com.termux/files/usr/bin/"
+    if [ cp ./ytfzf $termux_bin > /dev/null 2>&1 ]; then
         printf "finish to installing. Gam3 3e7. \n"
     else
         printf "Could not install. 9am3 0ver. \n"
