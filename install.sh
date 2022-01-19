f60c0d530d240dad3bfe2edc7c4e3297a43ec599
HexHelix-xx <rokuhara@protonmail.ch>
2022-01-19 23:22:07 +0900

    install.shを調整 typeに変換

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 run_run() {
     printf "[*] installing the ytfzf-termux"
     cp -v ./ytfzf /data/date/com.termux/files/usr/bin
-    if [ command -v ytfzf ]; then
+    if [ type "ytfzf" > /dev/null 2>&1 ]; then
         printf "finish to installing. Gam3 3e7. \n"
     else
         printf "Could not install. 9am3 0ver. \n"
