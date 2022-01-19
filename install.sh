7a2d386078007ae9f9de190f88e57ba694ccbe90
HexHelix-xx <rokuhara@protonmail.ch>
2022-01-19 23:09:41 +0900

    install.shを調整

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ run_run() {
     printf "[*] installing the ytfzf-termux"
     cp -v ./ytfzf /data/date/com.termux/files/usr/bin
     if [ command -v ytfzf ]; then
-        printf "finish to installing. Gam3 3e7."
+        printf "finish to installing. Gam3 3e7. \n"
     else
-        printf "Could not install. 9am3 0ver"
+        printf "Could not install. 9am3 0ver. \n"
     fi
 }
 run_run
